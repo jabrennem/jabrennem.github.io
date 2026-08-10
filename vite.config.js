6366e1cfc9b1f575934b84b9fccf70382a0e7b9a
Joshua Brenneman <Joshua.Brenneman@nationwidechildrens.org>
2026-08-10 12:37:38 -0400

    refactor blog part 4: production considerations and future additions

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import mdx from '@mdx-js/rollup';
 import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter from 'remark-mdx-frontmatter';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export default defineConfig({
   plugins: [
@@ -10,6 +11,9 @@ export default defineConfig({
       enforce: 'pre',
       ...mdx({
         remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+        rehypePlugins: [
+          [rehypePrettyCode, { theme: 'github-dark-dimmed', keepBackground: true }],
+        ],
       }),
     },
     react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
